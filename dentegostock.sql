a65e2f617d304438dcb850c6e2b4d0437331cb28
G-STOCK-HDV\Dentego <beatrice.djearamane@epsi.fr>
2017-04-20 11:09:37 +0200

    LE SQL A ENCORE CHANGÉ (surprise)

--- dentegostock.sql
+++ dentegostock.sql
@@ -1,6 +1,6 @@
 CREATE DATABASE dentegostock;
 
-USE DATABASE dentegostock;
+USE dentegostock;
 
 CREATE TABLE site
 (
@@ -10,22 +10,10 @@ CREATE TABLE site
   `CodePostal` varchar(50),
   `Ville` varchar(50),
   `Telephone` int(11),
-  `Fax` int(11),
+  `Fax` int(11)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 
-
-CREATE TABLE `historique` 
-(
-  `id` int(11) NOT NULL,
-  `Entree` tinyint(1) NOT NULL,
-  `Sortie` tinyint(1) NOT NULL,
-  `id_stock` int(11) NOT NULL,
-  `Quantite_initiale` int(11) NOT NULL,
-  `Quantite_modif` int(11) NOT NULL,
-  `DateModif` date NOT NULL,
-  CONSTRAINT FK_idstock FOREIGN KEY (id_stock) REFERENCES stock(id)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 CREATE TABLE `categorie`
 (
@@ -47,10 +35,10 @@ CREATE TABLE `fournisseur`
 
 CREATE TABLE `stock` 
 (
-  `id` int(11) PRIMARY KEY NOT NULL AUTO_INCREMENT
+  `id` int(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
   `NomObjet` varchar(250) NOT NULL,
   `Quantite` int(11) NOT NULL,
-  `PrixUnitaire` float(11) NOT NULL,
+  `PrixUnitaire` float NOT NULL,
   `id_site` int(11) NOT NULL,
   `id_cat` int(11) NOT NULL,
   `id_four` int(11) NOT NULL,
@@ -58,4 +46,16 @@ CREATE TABLE `stock`
   CONSTRAINT FK_idsite FOREIGN KEY (id_site) REFERENCES site(id),
   CONSTRAINT FK_idcat FOREIGN KEY (id_cat) REFERENCES categorie(id),
   CONSTRAINT FK_idfour FOREIGN KEY (id_four) REFERENCES fournisseur(id)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+CREATE TABLE `historique` 
+(
+  `id` int(11) NOT NULL,
+  `Entree` tinyint(1) NOT NULL,
+  `Sortie` tinyint(1) NOT NULL,
+  `id_stock` int(11) NOT NULL,
+  `Quantite_initiale` int(11) NOT NULL,
+  `Quantite_modif` int(11) NOT NULL,
+  `DateModif` date NOT NULL,
+  CONSTRAINT FK_idstock FOREIGN KEY (id_stock) REFERENCES stock(id)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
