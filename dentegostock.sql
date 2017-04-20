e177a46a2e64a566877c542635e0cd8bc985346e
G-STOCK-HDV\Dentego <beatrice.djearamane@epsi.fr>
2017-04-20 10:06:31 +0200

    LE SQL A ENCORE CHANGÉ TROP BIEN MDR

--- dentegostock.sql
+++ dentegostock.sql
@@ -20,7 +20,7 @@ CREATE TABLE `historique`
   `id` int(11) NOT NULL,
   `Entree` tinyint(1) NOT NULL,
   `Sortie` tinyint(1) NOT NULL,
-  `id_stock` varchar(11) NOT NULL,
+  `id_stock` int(11) NOT NULL,
   `Quantite_initiale` int(11) NOT NULL,
   `Quantite_modif` int(11) NOT NULL,
   `DateModif` date NOT NULL,
@@ -47,13 +47,14 @@ CREATE TABLE `fournisseur`
 
 CREATE TABLE `stock` 
 (
-  `id` varchar(11) PRIMARY KEY NOT NULL AUTO_INCREMENT
-  `NomObjet` varchar(150) NOT NULL,
+  `id` int(11) PRIMARY KEY NOT NULL AUTO_INCREMENT
+  `NomObjet` varchar(250) NOT NULL,
   `Quantite` int(11) NOT NULL,
   `PrixUnitaire` float(11) NOT NULL,
   `id_site` int(11) NOT NULL,
   `id_cat` int(11) NOT NULL,
   `id_four` int(11) NOT NULL,
+  `codearticle` int(11) NOT NULL,
   CONSTRAINT FK_idsite FOREIGN KEY (id_site) REFERENCES site(id),
   CONSTRAINT FK_idcat FOREIGN KEY (id_cat) REFERENCES categorie(id),
   CONSTRAINT FK_idfour FOREIGN KEY (id_four) REFERENCES fournisseur(id)
