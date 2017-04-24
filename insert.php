ab327ea039ff44b13589c9dbf5e965c8e9e1df2c
TP-03\Dentego <lealumbroso29@gmail.com>
2017-04-24 16:00:36 +0200

    MCD rdv>prat>periode au 24 avril

--- insert.php
+++ insert.php
@@ -1,9 +1,21 @@
 
-emlfkhjlkml
+
+
 
 <?php
 
-echo $_POST['quantite'];
-
-
+$quanti=$_POST["quantite"];
+ $con = mysqli_connect("localhost","root","","dentegostock");
+            if (mysqli_connect_errno())
+              {
+                      echo "Failed to connect to MySQL: " . mysqli_connect_error();
+              }
+$sql = "INSERT INTO historique (Quantite_modif) VALUES (".$quanti.") ";
+      $result = $con->query($sql);
+      if ($result===true)
+      	{
+                      echo "Execution réussie";
+              }
+      else
+      	{echo "Execution echouee";}
 ?>
